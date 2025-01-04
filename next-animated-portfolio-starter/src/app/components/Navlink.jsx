8fa04df0991f0b41f988807dcd591488fca21ecb
rumesh <rumeshk066@gmail.com>
2025-01-04 12:34:06 +0530

    navLink coding done

--- next-animated-portfolio-starter/src/app/components/Navlink.jsx
+++ next-animated-portfolio-starter/src/app/components/Navlink.jsx
@@ -7,7 +7,8 @@ const Navlink = ({link}) => {
     const pathname = usePathname();
     console.log(pathname);
   return (
-    <Link className={`rounded p-1 ${pathname === link.url && "bg-black text-white"}`} href={link.url}>
+    <Link className={`rounded-xl px-4 py-2 ${pathname === link.url ? "bg-[#252c44] text-white" : ""}`} href={link.url}>
+        {link.title}
     </Link>
   )
 }
