5d8ff7f9897e54bc52cf29849793d836521f87f9
rumesh <rumeshk066@gmail.com>
2025-01-28 21:06:04 +0530

    commit-1

--- next-animated-portfolio-starter/src/app/about/Page.jsx
+++ next-animated-portfolio-starter/src/app/about/Page.jsx
@@ -13,9 +13,17 @@ const AboutPage = () => {
       {/* CONTAINER */}
       <div className="">
         {/* TEXT CONTAINER */}
-        <div className="flex flex-col gap-24 p-4 text-xl sm:p-8 md:p-12 lg:p-20 xl:p-">
+        <div className="flex flex-col gap-24 p-4 text-xl sm:p-8 md:p-12 lg:p-20 xl:p-48 md:gap-32 lg:gap-48 xl:gap-64">
           {/* BIOGRAPHY CONTAINER */}
-          <div className="">BIOGRAPHY</div>
+          <div className="flex flex-col justify-center gap-12">
+            <h1>BIOGRAPHY</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis illum eligendi fuga id. Labore nisi ea dignissimos ipsa, itaque cupiditate ducimus quasi sit tenetur enim, quidem eius? Earum, atque quis?
+            </p>
+            <span>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </span>
+          </div>
           {/* SKILLS CONTAINER */}
           <div className="">SKILLS</div>
           {/* EXPERIENCE CONTAINER */}
